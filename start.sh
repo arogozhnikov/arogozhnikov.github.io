6ab0bd1b549d6b0d912b80f86f9b95f5b4b30ba1
Alex Rogozhnikov <iamfullofspam@gmail.com>
2023-07-13 20:26:43 -0700

    more comments

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-docker run -it --rm --label=jekyll --volume=`pwd`:/srv/jekyll  -it -p 4000:4000 younglook/jekyll-arm64 \
+# use docker backend for this, DO NOT USE colima, it can't do incrementals
+docker run -it --rm --label=jekyll --volume=`pwd`:/srv/jekyll --mount type=tmpfs,destination=/srv/jekyll/_site -it -p 4000:4000 younglook/jekyll-arm64 \
  bash -c 'jekyll build && jekyll serve --incremental'
 
 
 # docker run -it --rm --label=jekyll --volume=`pwd`:/srv/jekyll  -it -p 4000:4000 jekyll/jekyll bash
 # jekyll serve --incremental
+
+## Another way is using nix-shell
+# nix-shell -p jekyll
+# jekyll serve
